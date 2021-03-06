728cbd4d2274e7ee37b7a3714b4d1a458af531a2
f6wbl6 <tera81318@gmail.com>
2021-03-06 14:58:26 +0900

    fix GA

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-GOOGLE_APPLICATION_CREDENTIALS_JSON=/tmp/credentials.json
+GOOGLE_APPLICATION_CREDENTIALS_JSON=./tmp/credentials.json
 echo "${GOOGLE_APPLICATION_CREDENTIALS}" > "${GOOGLE_APPLICATION_CREDENTIALS_JSON}"
 gcloud auth activate-service-account "${SA_EMAIL}" --key-file="${GOOGLE_APPLICATION_CREDENTIALS_JSON}"
 

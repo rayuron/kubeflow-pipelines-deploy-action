581b186192d120f52b741b7f54a3876bdfde8be8
f6wbl6 <tera81318@gmail.com>
2021-03-14 21:28:34 +0900

    fix README.md

--- src/main.py
+++ src/main.py
@@ -161,7 +161,7 @@ def main():
         logging.info(f"A run is created with: {job_name}")
 
     # Set recurring run
-    if os.getenv("INPUT_RUN_RECURRING_PIPELINE") == "true":
+    if os.getenv("INPUT_RECURRING_CRON_EXPRESSION") is not None:
         job_name = f"Recurring_run_{pipeline_name}_on_{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}"
         client.create_recurring_run(
             pipeline_id=pipeline_id,

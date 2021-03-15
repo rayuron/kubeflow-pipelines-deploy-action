55ee3bd3b9bbb3f2e7c3aeec81c8febd1d23e485
f6wbl6 <tera81318@gmail.com>
2021-03-16 00:32:43 +0900

    bug fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 GOOGLE_APPLICATION_CREDENTIALS=./credentials.json
 echo "${GOOGLE_APPLICATION_CREDENTIALS_JSON}" > "${GOOGLE_APPLICATION_CREDENTIALS}"
-gcloud auth activate-service-account "${SA_EMAIL}" --key-file="${GOOGLE_APPLICATION_CREDENTIALS}"
+echo "${GOOGLE_APPLICATION_CREDENTIALS_JSON}" | gcloud auth activate-service-account "${SA_EMAIL}" --key-file=-
 
 python  ./src/main.py

a77b4faba558dcf0e22d447788ee12c4eaf7bc55
f6wbl6 <tera81318@gmail.com>
2021-03-17 00:38:32 +0900

    fix github actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 GOOGLE_APPLICATION_CREDENTIALS=/tmp/credentials.json
-echo "${ENCODED_GOOGLE_APPLICATION_CREDENTIALS_JSON}" | base64 -d > "${GOOGLE_APPLICATION_CREDENTIALS}"
+echo "${GOOGLE_APPLICATION_CREDENTIALS_JSON}" > "${GOOGLE_APPLICATION_CREDENTIALS}"
 gcloud auth activate-service-account "${SA_EMAIL}" --key-file="${GOOGLE_APPLICATION_CREDENTIALS}"
 
-python  ./src/main.py
+python ./src/main.py

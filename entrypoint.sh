727bcd231503ed750f705eca6a020db1792244d7
f6wbl6 <tera81318@gmail.com>
2021-03-23 20:10:00 +0900

    fix shell

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ export GOOGLE_APPLICATION_CREDENTIALS=/tmp/credentials.json
 echo "${GOOGLE_APPLICATION_CREDENTIALS_JSON}" > "${GOOGLE_APPLICATION_CREDENTIALS}"
 gcloud auth activate-service-account "${SA_EMAIL}" --key-file="${GOOGLE_APPLICATION_CREDENTIALS}"
 
-python ./src/main.py
+python /app/src/main.py
